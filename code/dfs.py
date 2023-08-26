e345f7ed3332fa3466890d3550aa35abe5775113
Nameless-itf23 <ohutoncat@gmail.com>
2023-08-26 22:59:52 +0900

    add dfs

---
+++ code/dfs.py
@@ -0,0 +1,31 @@
+# this code is incomplete as template.
+
+import sys
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+n, m = map(int, input().split())
+g = [[] for _ in range(n)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    a -= 1
+    b -= 1
+    g[a].append([b, c])
+    g[b].append([a, c])
+
+used = [False] * n
+ans = 0
+def dfs(v, s):
+    global used, ans
+    used[v] = True
+    for i in g[v]:
+        if not used[i[0]]:
+            dfs(i[0], s+i[1])
+    ans = max(s, ans)
+    used[v] = False
+
+for i in range(n):
+    used = [False] * n
+    dfs(i,0)
+
+print(ans)
--- code/dijkstra.py
+++ code/dijkstra.py
@@ -1,25 +1,27 @@
-# import heapq
+# this code is incomplete as template.
 
-# g = [[] for _ in range(16)]
-# for _ in range(24):
-#     _s, _g, _d = map(int, input().split())
-#     g[_s].append([_d, _g])
-#     g[_g].append([_d, _s])
+import heapq
 
-# def dijkstra():
-#     d = [10**10] * 16
-#     d[0] = 0
-#     q = [[0, 0]]
-#     while q:
-#         n_d, n_v = heapq.heappop(q)
-#         if n_d > d[n_v]:
-#             continue
-#         for v in g[n_v]:
-#             tmp = n_d + v[0]
-#             if d[v[1]] > tmp:
-#                 d[v[1]] = tmp
-#                 heapq.heappush(q, [tmp, v[1]])
-#     return d
+g = [[] for _ in range(16)]
+for _ in range(24):
+    _s, _g, _d = map(int, input().split())
+    g[_s].append([_d, _g])
+    g[_g].append([_d, _s])
 
-# ans = dijkstra()
-# print(f'ans:{ans[15]}')
+def dijkstra():
+    d = [10**10] * 16
+    d[0] = 0
+    q = [[0, 0]]
+    while q:
+        n_d, n_v = heapq.heappop(q)
+        if n_d > d[n_v]:
+            continue
+        for v in g[n_v]:
+            tmp = n_d + v[0]
+            if d[v[1]] > tmp:
+                d[v[1]] = tmp
+                heapq.heappush(q, [tmp, v[1]])
+    return d
+
+ans = dijkstra()
+print(f'ans:{ans[15]}')
