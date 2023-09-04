5c4ac24f684b6cb19341e9ee301862d004a34437
Nameless-itf23 <ohutoncat@gmail.com>
2023-09-04 14:11:03 +0900

    .

--- code/frac.py
+++ code/frac.py
@@ -1,6 +1,6 @@
 # 逆元のmod
 
 const = 998244353
-def frac(a, b): # a/b
+def frac(num1, num2): # num1 / num2
     global const
-    return a*pow(b, -1, const)%const
+    return num1 * pow(num2, -1, const) % const
